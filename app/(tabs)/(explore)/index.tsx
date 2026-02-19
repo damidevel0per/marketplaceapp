8276859adf9885209211f8ab40aba2e4035f28fc
damidevel0per <damidevtech@gmail.com>
2026-02-19 14:09:23 -0300

    instalando libreria React native paper

--- app/(tabs)/(explore)/index.tsx
+++ app/(tabs)/(explore)/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import {View, Text} from "react-native";
 import { Link } from "expo-router";
+import {Button} from "react-native-paper";
 
 export default function ExploreScreen() {
   return (
@@ -9,6 +10,10 @@ export default function ExploreScreen() {
       <Text>Explore Screen</Text>
       <Link href={'/(tabs)/(explore)/store/1'}>Ir a Store 1</Link>
       <Link href={'/(tabs)/(explore)/store/100'}>Ir a Store 100</Link>
+      
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
     </View>
   );
 }
